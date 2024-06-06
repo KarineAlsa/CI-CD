96a70ba96e3155374dcb7bf0569ab60f9340325f
KarineAlsa <saremana@hotmail.com>
2024-06-06 03:36:14 -0600

    tiene que funcionar v4

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/api/v1/welcome', (req,res) => {
-	res.send('Hello World! actualizacion3');
+	res.send('Hello World! actualizacion4');
 });
 
 app.listen(port,()=>{
